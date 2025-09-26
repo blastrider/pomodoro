c4d928f5f09be86ba0eb6877df8b7be3fccb7bb2
Maxime Guillemin <guimaxali@gmail.com>
2025-09-26 13:05:37 +0200

    change the function

--- tests/schedule_integration.rs
+++ tests/schedule_integration.rs
@@ -1,8 +1,8 @@
-use pomodoro::domain::schedule::Schedule;
+use pomodoro_cli::domain::schedule::Schedule;
 
 #[test]
 fn schedule_length_for_2_cycles() {
-    let s = Schedule::from_minutes_for_test(1, 1, 1, 2);
+    let s = Schedule::from_seconds_for_test(1, 1, 1, 2);
     // 2 focuses + 1 short + 1 long = 4 segments
     assert_eq!(s.segments.len(), 4);
 }
